fddfccaf80ddd895d4f53d83235475bb85f84135
R0ahulkd <R0ahulkd@gmail.com>
2026-04-03 23:54:30 +0530

    Type Conversion in Java

---
+++ 01_Java_Basics/Day 4/typeconversion.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class typeconversion {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float myFloat = 5;
+        System.out.println(myFloat);
+
+        int myInt = (int) 5.45f;
+        System.out.println(myInt);
+    }
+}
