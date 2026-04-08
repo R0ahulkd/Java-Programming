e25e337b001036f979f7e70311b98d29e80e9e6c
R0ahulkd <R0ahulkd@gmail.com>
2026-04-08 23:41:55 +0530

    Functions

---
+++ 01_Java_Basics/Day 8/Functions.java
@@ -0,0 +1,8 @@
+public class Functions {
+    public static void main(String[] args) {
+        greetings();
+    }
+    public static void greetings() {
+        System.out.println("Hello");
+    }
+}
