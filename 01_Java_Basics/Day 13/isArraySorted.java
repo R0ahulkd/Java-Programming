e63e7b368055ee04337c1b8c8a1805c69b6b989f
R0ahulkd <R0ahulkd@gmail.com>
2026-04-14 18:21:47 +0530

    Array Sorted Updated

--- 01_Java_Basics/Day 13/isArraySorted.java
+++ 01_Java_Basics/Day 13/isArraySorted.java
@@ -1,21 +1,47 @@
 public class isArraySorted {
     public static void main(String[] args) {
         int[] myArr = ArrayInputUtility.inputArray();
-        Boolean result = isSorted(myArr);
-        if (result){
+        if (isSorted(myArr) == 1) {
             System.out.println("Array is Sorted");
         } else {
             System.out.println("Array is not Sorted");
         }
     }
-    public static boolean isSorted(int[] arr) {
-        int i = 1;
-        while (i < arr.length) {
-            if (arr[i-1] > arr[i]) {
-                return false;
+    public static int isSorted(int[] Arr) {
+        int i = 0;
+        int min = Arr[0];
+        while (i < Arr.length) {
+            if (min > Arr[i]) {
+                min = Arr[i];
             }
             i++;
         }
-        return true;
+        if (min == Arr[0]) {
+            return isAsc(Arr);
+        } else if (min == Arr[Arr.length-1]) {
+            return isDesc(Arr);
+        } else {
+            return 0;
+        }
+    }
+    public static int isAsc(int[] Arr) {
+        int i = 1;
+        while (i < Arr.length) {
+            if (Arr[i-1] > Arr[i]) {
+                return 0;
+            }
+            i++;
+        }
+        return 1;
+    }
+    public static int isDesc(int[] Arr) {
+        int i = 1;
+        while (i < Arr.length) {
+            if (Arr[i-1] < Arr[i]) {
+                return 0;
+            }
+            i++;
+        }
+        return 1;
     }
 }
