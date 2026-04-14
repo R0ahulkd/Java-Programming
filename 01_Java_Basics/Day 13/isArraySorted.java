db482578569c8431c0a91edfe3f3bf37fbc126cd
R0ahulkd <R0ahulkd@gmail.com>
2026-04-14 17:01:03 +0530

    Check Array is Sorted or Not

--- 01_Java_Basics/Day 13/isArraySorted.java
+++ 01_Java_Basics/Day 13/isArraySorted.java
@@ -1,3 +1,21 @@
 public class isArraySorted {
-    public
+    public static void main(String[] args) {
+        int[] myArr = ArrayInputUtility.inputArray();
+        Boolean result = isSorted(myArr);
+        if (result){
+            System.out.println("Array is Sorted");
+        } else {
+            System.out.println("Array is not Sorted");
+        }
+    }
+    public static boolean isSorted(int[] arr) {
+        int i = 1;
+        while (i < arr.length) {
+            if (arr[i-1] > arr[i]) {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
 }
