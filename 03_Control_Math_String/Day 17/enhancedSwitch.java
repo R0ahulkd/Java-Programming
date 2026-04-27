6f43bc0e919f80ac882fde17fa2cd984f62e4ac5
R0ahulkd <R0ahulkd@gmail.com>
2026-04-27 22:59:07 +0530

    Enhanced Switch

---
+++ 03_Control_Math_String/Day 17/enhancedSwitch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class enhancedSwitch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Number 1 - 7 : ");
+        int day = input.nextInt();
+
+        String output = switch (day) {
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 7 -> "Sunday";
+            default -> "Invalid Input";
+        };
+        System.out.println(output);
+    }
+}
