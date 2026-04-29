6255ede3002c98e9e7cff5f34364dd721e6b5fab
R0ahulkd <R0ahulkd@gmail.com>
2026-04-29 22:49:08 +0530

    Factorial Using Recursion

---
+++ 03_Control_Math_String/Day 19/Recursion1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Recursion1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Number you need to calculate Factorial : ");
+        int num = input.nextInt();
+        System.out.println(Factorial(num));
+    }
+    public static long Factorial(int num) {
+        System.out.println("Function called for : " + num);
+        if (num == 1) {
+            return 1;
+        }
+        return num * Factorial(num-1);
+    }
+}
