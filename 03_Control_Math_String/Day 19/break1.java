a3fc5fc892369cbe6243933c1408db960ba72739
R0ahulkd <R0ahulkd@gmail.com>
2026-05-05 16:51:03 +0530

    Break Practice

--- 03_Control_Math_String/Day 19/break1.java
+++ 03_Control_Math_String/Day 19/break1.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class break1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String in;
-        boolean start = true;
-        while (start == true) {
-            System.out.print("Please Enter Input : ");
-            in = input.nextLine();
-            System.out.println(in);
-            if (in == "exit") {
-                start = false;
+        while (true) {
+            System.out.print("Please enter a command : ");
+            String command = input.next();
+            if (command.equals("exit")) {
+                break;
             }
         }
     }
