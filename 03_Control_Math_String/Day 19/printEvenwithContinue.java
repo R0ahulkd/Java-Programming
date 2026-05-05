9ce62b3110ccc246d719a97693727d7fb0d0ae81
R0ahulkd <R0ahulkd@gmail.com>
2026-05-05 17:05:56 +0530

    Even number using Continue

---
+++ 03_Control_Math_String/Day 19/printEvenwithContinue.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class printEvenwithContinue {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i%2 != 0) {
+                continue;
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
