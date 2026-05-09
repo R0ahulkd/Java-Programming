790ee9642d55db9e1bcfe5cc13d80029c62fdcae
R0ahulkd <R0ahulkd@gmail.com>
2026-05-09 16:28:38 +0530

    Final keyword Initialization using Constructor

---
+++ 03_Control_Math_String/Day 21/FinalCar4.java
@@ -0,0 +1,15 @@
+public class FinalCar4 {
+    final int noOfWheels;
+    final String model;
+    final String engineInLiters;
+
+    public FinalCar4(int noOfWheels, String model, String engineInLiters) {
+        this.noOfWheels = noOfWheels;
+        this.model = model;
+        this.engineInLiters = engineInLiters;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
