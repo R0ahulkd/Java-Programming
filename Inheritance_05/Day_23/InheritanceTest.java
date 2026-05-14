6200d3d4188a2bdb0b084f8bd6c58e371b519253
R0ahulkd <R0ahulkd@gmail.com>
2026-05-14 23:16:28 +0530

    Equals and Hash Code in java

--- Inheritance_05/Day_23/InheritanceTest.java
+++ Inheritance_05/Day_23/InheritanceTest.java
@@ -4,6 +4,9 @@ public class InheritanceTest {
     public static void main(String[] args) {
         Vehicle vehicle = new Vehicle();
         vehicle.commute();
+        System.out.println(vehicle.toString());
+        System.out.println(vehicle.hashCode());
+        System.out.println(vehicle.getClass() );
 
         TwoWheeler two = new TwoWheeler();
         two.commute();
