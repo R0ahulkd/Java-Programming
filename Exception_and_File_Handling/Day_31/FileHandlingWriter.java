89032b84126a17490782c964d5e39c4d342347df
R0ahulkd <R0ahulkd@gmail.com>
2026-06-01 23:14:31 +0530

    File Reading

--- Exception_and_File_Handling/Day_31/FileHandlingWriter.java
+++ Exception_and_File_Handling/Day_31/FileHandlingWriter.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileHandlingWriter {
-    
+
     public static void main(String[] args) {
         String fileName = "java-course.txt";
         try (FileWriter writer = new FileWriter(fileName);) {
